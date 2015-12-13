2fc00db97b1515836cc32022db12ab972e3c3e46
rahul sahay <rahulsahay19@gmail.com>
2015-12-13 18:51:17 +0530

    2nd Commit

--- Interface-Demo.Console/Program.cs
+++ Interface-Demo.Console/Program.cs
@@ -12,9 +12,22 @@ namespace Interface_Demo.Console
             var squareAbstract= new SquareAbstract(4,4);
             DisplayDimensions("Square with abstract Implementation",squareAbstract);
 
+            var circleInterface= new CircleInterface(5);
+            DisplayCircle("Circle with Interface Implementation", circleInterface);
+
             System.Console.ReadLine();
         }
 
+        private static void DisplayCircle(string objectType, CircleInterface circleInterface)
+        {
+            System.Console.WriteLine("Displaying Dimensions of " + objectType);
+            System.Console.WriteLine("Circle with Radius:- " + circleInterface.radius);
+            System.Console.WriteLine("Area of " + objectType + " is " + circleInterface.CalcArea());
+            System.Console.WriteLine("Perimeter of " + objectType + " is " + circleInterface.CalcPerimeter());
+            System.Console.WriteLine("==============================================================");
+        }
+
+        //dynamic key will resolve an object at runtime. very useful keyword
         private static void DisplayDimensions(string objectType, dynamic square)
         {
             System.Console.WriteLine("Displaying Dimensions of " +objectType);
